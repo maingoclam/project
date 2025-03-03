62d4d7f8390b3cc0cc246d1b9b283e32b5f40298
maingoclam <maingoclam2003@gmail.com>
2025-03-03 13:49:46 +0700

    lan 2

--- add_username.js
+++ add_username.js
@@ -3,3 +3,8 @@ console.log("Trang thêm UserName đã sẵn sàng!");
 function handleUsernameInput(username) {
   console.log("UserName nhập vào: ", username);
 }
+console.log("Trang thêm Mật Khẩu đã sẵn sàng!");
+
+function handlePasswordInput(password) {
+    console.log("Mật khẩu nhập vào: ", password);
+}
